a891e7cda765154facfff34f9beb5bbd45f9c5ec
amyra7160-glitch <amyra7160@gmail.com>
2026-03-12 03:52:20 +0300

    Create configuration file for Age of Destruction

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// Configuration for Age of Destruction Game
+export const config = { 
+    gameTitle: 'Age of Destruction',
+    maxPlayers: 4,
+    startingResources: {
+        gold: 100,
+        wood: 50,
+        stone: 50
+    },
+    mapSettings: {
+        terrainTypes: ['plain', 'forest', 'mountain'],
+        size: 'medium'
+    },
+    difficultyLevels: ['easy', 'medium', 'hard'],
+    version: '1.0.0',
+    releaseDate: '2026-03-12'
+};
